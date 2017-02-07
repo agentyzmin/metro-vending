3da0b213615a816be561c4dd4cba2d58902d8b99
Alexander Zaytsev <nqst@MacBook-Air-Alexander.local>
2017-02-07 17:55:48 +0200

    basic structure added

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var child = require('child_process');
+var gutil = require('gulp-util');
+
+var siteRoot = '_site';
+var cssFiles = '_scss/**/*.*';
+
+gulp.task('css', () => {
+  gulp.src(cssFiles)
+   .pipe(sass())
+   .pipe(postcss([ autoprefixer() ]))
+   .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('jekyll', () => {
+  var jekyll = child.spawn('jekyll', ['build',
+    '--watch',
+    '--incremental'
+  ]);
+
+  var jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('watch', () => {
+  gulp.watch(cssFiles, ['css']);
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 5050,
+    server: {
+      baseDir: siteRoot
+    },
+    open: false,
+    notify: false
+  });
+
+  gulp.watch(cssFiles, ['css']);
+});
+
+gulp.task('default', ['css', 'jekyll', 'serve']);
